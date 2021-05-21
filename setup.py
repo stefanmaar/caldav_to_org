29fa0c6e84eff06b8e47986ee3d8b28006bc2a6c
Stefan Mertl <info@stefanmertl.com>
2021-05-21 12:43:54 +0200

    Updated the setup properties.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-package = "org-mode-agenda"
-version = "0.4"
+package = "caldav_to_org"
+version = "0.1"
 
 with open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
@@ -9,19 +9,19 @@ with open("README.rst", encoding="utf-8") as f:
 setup(
     name=package,
     version=version,
-    author="Óscar Nájera",
-    author_email="hi@oscarnajera.com",
+    author="Óscar Nájera, Stefan Mertl",
+    author_email="hi@oscarnajera.com,stefan@mertl-research.at",
     description="Import icalendar agendas to Orgmode",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://gitlab.com/Titan-C/org-mode-agenda",
-    packages=["org_agenda"],
-    entry_points={"console_scripts": ["org_agenda_sync = org_agenda:main"]},
+    url="https://github.com/stefanmaar/caldav_to_org.git",
+    packages=["caldav_to_org"],
+    entry_points={"console_scripts": ["org_caldav_import = caldav_to_org:main"]},
     install_requires=["requests", "icalendar", "tzlocal", "vobject", "aiohttp"],
     license="GNU General Public License v3 or later (GPLv3+)",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
